5a8b3b946226bdeb4bdaf7ec8f156eb926511319
creatersources <108732488+creatersources@users.noreply.github.com>
2022-08-12 17:40:48 +0530

    Update ami.js
    Rahul

--- ami.js
+++ ami.js
@@ -1,126 +1,37 @@
-var pane = $('#pilotGame'),
-  box = $('#outerPlane'),
-  ground = $('#pilotGround'),
-  wh = pane.width() - box.width(),
-  wv = pane.height() - box.height(),
-  d = {},
-  x = 10,
-  g = 0;
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
-function flight2() {
-  function newh(v, a, b) {
-    var n = parseInt(v, 10) - (d[a] ? 0 : 0) + (d[b] ? 0 : 0);
-    return n < 50 ? 50 : n > wh - 25 ? wh - 25 : n;
-  }
+const newYears = "1 Jan 2023";
 
-  function newv(v, a, b) {
-    var n = parseInt(v, 10) - (d[a] ? 0 : g) + (d[b] ? 0 : 0);
-    return n < 100 ? 100 : n > wv ? wv : n;
-  }
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
 
-  $(window).keydown(function (e) {
-    d[e.which] = true;
-    ready = true;
-    if (ready == true) {
-      var showName = 'showName';
-      // Basic Moves
-      if (e.which == 37) {
-        document.getElementById('plane').classList.add('dblFlip');
-        document.getElementById('nombre').classList.add('showName');
-        document.getElementById('nombre').innerHTML = 'Double Flip';
-        setTimeout(function () {
-          document.getElementById('nombre').classList.remove('showName');
-          document.getElementById('plane').classList.remove('dblFlip');
-          ready = true;
-        }, 1250);
-      }
-      if (e.which == 38) {
-        document.getElementById('plane').classList.add('loop');
-        document.getElementById('nombre').classList.add('showName');
-        document.getElementById('nombre').innerHTML = 'Loop';
-        setTimeout(function () {
-          document.getElementById('nombre').classList.remove('showName');
-          document.getElementById('plane').classList.remove('loop');
-          ready = true;
-        }, 1250);
-      }
-      if (e.which == 39) {
-        document.getElementById('plane').classList.add('circle');
-        document.getElementById('nombre').classList.add('showName');
-        document.getElementById('nombre').innerHTML = 'Circle';
-        setTimeout(function () {
-          document.getElementById('nombre').classList.remove('showName');
-          document.getElementById('plane').classList.remove('circle');
-          ready = true;
-        }, 1250);
-      }
-      if (e.which == 40) {
-        document.getElementById('plane').classList.add('roll');
-        document.getElementById('nombre').classList.add('showName');
-        document.getElementById('nombre').innerHTML = 'Barrel Roll';
-        setTimeout(function () {
-          document.getElementById('nombre').classList.remove('showName');
-          document.getElementById('plane').classList.remove('roll');
-          ready = true;
-        }, 1250);
-      }
-      if (
-        document.getElementsByClassName('dblFlip')[0] &&
-        document.getElementsByClassName('circle')[0]
-      ) {
-        document.getElementById('nombre').innerHTML = 'Flippy McCircle';
-      }
-      if (
-        document.getElementsByClassName('dblFlip')[0] &&
-        document.getElementsByClassName('loop')[0]
-      ) {
-        document.getElementById('nombre').innerHTML = 'Loop²';
-      }
-      if (
-        document.getElementsByClassName('dblFlip')[0] &&
-        document.getElementsByClassName('roll')[0]
-      ) {
-        document.getElementById('nombre').innerHTML =
-          'The Ultimate Barrel Roll';
-      }
-      if (
-        document.getElementsByClassName('roll')[0] &&
-        document.getElementsByClassName('loop')[0]
-      ) {
-        document.getElementById('nombre').innerHTML = 'Loopy McNutty';
-      }
-      if (
-        document.getElementsByClassName('roll')[0] &&
-        document.getElementsByClassName('circle')[0]
-      ) {
-        document.getElementById('nombre').innerHTML = 'Speed Racer';
-      }
-      if (
-        document.getElementsByClassName('circle')[0] &&
-        document.getElementsByClassName('loop')[0]
-      ) {
-        document.getElementById('nombre').innerHTML = 'Topsy Turvy';
-      }
-    }
-  });
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-  $(window).keyup(function (e) {
-    d[e.which] = false;
-    ready = false;
-    setTimeout(function () {
-      ready = true;
-    }, 1250);
-  });
-  box.css({
-    left: function (i, v) {
-      return newh(v, 37, 39);
-    },
-    top: function (i, v) {
-      return newv(v, 38, 40);
-    },
-  });
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
-  wh = pane.width() - box.width();
-  wv = pane.height() * 0.95 - box.height();
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
-flyTricks = setInterval(flight2, 1000 / 30);
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
+
+
+
+
+
